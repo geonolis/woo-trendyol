ebee2f526b56e00d62b10172a9edd5797982ae22
geonolis <geonolis@hotmail.com>
2026-09-10 18:38:33 +0000

    Revert "Add Holiday Mode setting with smart change-tracking queue and automatic catch-up sync"
    This reverts commit 9cf55fb767c2480212cc53c9ed2c57e5e3e52367.

--- includes/class-woo-trendyol-activator.php
+++ includes/class-woo-trendyol-activator.php
@@ -36,7 +36,6 @@ class Woo_Trendyol_Activator {
         // Set default options only if they do not already exist.
         $defaults = [
             'trendyol_api_active'          => 'no',
-            'trendyol_holiday_mode'        => 'no',
             'trendyol_seller_id'           => '',
             'trendyol_api_key'             => '',
             'trendyol_api_secret'          => '',
